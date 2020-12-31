4d94e5ce4ca5c632142d43dfb1d6de2d2d064520
Atiqur <atiqrs.me@gmail.com>
2020-12-31 21:01:04 +0600

    Login page complete

--- app/src/main/java/com/atiqrs/demotaskatiqur/Form.java
+++ app/src/main/java/com/atiqrs/demotaskatiqur/Form.java
@@ -3,12 +3,23 @@ package com.atiqrs.demotaskatiqur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Form extends AppCompatActivity {
-
+    private EditText nameForm,mobileForm,emailForm;
+    private Button submitButtonForm,skipButtonForm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        Log.d("info","Form class");
+        nameForm = findViewById(R.id.nameForm);
+        mobileForm = findViewById(R.id.mobileForm);
+        emailForm = findViewById(R.id.emailForm);
+        submitButtonForm = findViewById(R.id.submitButtonForm);
+        skipButtonForm = findViewById(R.id.skipButtonForm);
     }
 }
--- app/src/main/java/com/atiqrs/demotaskatiqur/MainActivity.java
+++ app/src/main/java/com/atiqrs/demotaskatiqur/MainActivity.java
@@ -2,13 +2,39 @@ package com.atiqrs.demotaskatiqur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText loginUsername,loginPass;
+    private Button logInButton;
+    protected String USERNAME = "admin";
+    protected String PASSWORD = "admin1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginUsername= findViewById(R.id.loginUsername);
+        loginPass= findViewById(R.id.loginPass);
+        logInButton= findViewById(R.id.logInButton);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = loginUsername.getText().toString();
+                String password = loginPass.getText().toString();
+                if (username.equals(USERNAME) && password.equals(PASSWORD)){
+                    Intent intent = new Intent(getApplicationContext(),Form.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
